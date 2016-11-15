8bf4a4effce27f0a63e1069df7ee3362c6fcc78f
Yearup <yearup@Yearups-Mac-mini-2.local>
2016-11-15 15:03:47 -0800

    added event listener and alert

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
 	$('h2').html("Join me as we take a look at music!");
 	$('h2').css("color", "grey");
 	$('h1').css("color", "pink");
+	$('h2').on("click", function(){
+		alert("Thanks for visiting my page!");
+	});
 
 
 });
